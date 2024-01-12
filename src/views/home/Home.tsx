57f8ba925986cefd879aedcbd39b5fe883b15c54
FernandoNasso <fernando.nasso@gmail.com>
2024-01-12 10:31:29 -0300

    add Header

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProperties, selectProperties } from '../../redux/propertySlice';
 import Cards from '../../components/cards/Cards'; // Asegúrate de tener la importación correcta
+import Header from '../../components/header/Header';
 
 const Home: React.FC = () => {
   const dispatch = useDispatch();
@@ -14,6 +15,7 @@ const Home: React.FC = () => {
 
   return (
     <div>
+      <Header />
       <Cards properties={properties} />
     </div>
   );
